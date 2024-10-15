639fc7cd8c9524c4d54ea45addc137c2d4853871
Yaoxing <yaoxing.zhang@gmail.com>
2024-10-15 23:59:11 +0200

    + all functions finished

---
+++ train_info.py
@@ -0,0 +1,171 @@
+from textual.app import App
+from textual.containers import ScrollableContainer
+from textual.widgets import *
+from lib.traffic_api import *
+from textual.css._style_properties import OffsetProperty
+from datetime import datetime
+import pytz
+
+logger = logging.getLogger(__name__)
+
+class MessageLabel(Label):
+    position = config["message"]["margin"]
+    def scroll(self):
+        margin = config["message"]["margin"]
+        content_w = self.parent.get_visible_size().width
+        self.position -= 1
+        if self.position < 0 and self.size.width + self.position >= content_w:
+            self.styles.offset = self.position, 0
+        if self.size.width + self.position < content_w - margin:
+            self.styles.offset = 0, 0
+            self.position = config["message"]["margin"]
+    
+    def on_mount(self):
+        self.set_interval(config["message"]["scrollSpeed"], self.scroll)
+
+class TrainStationMessage(Static):
+    def load_message(self):
+        lag_message = []
+        normal_message = []
+        # lag_message.append({
+        #     "FreeText": "Tågtrafiken är åter igång efter tidigare \nstopp mellan Farsta Strand och Årstaberg.\nFörseningar kvarstår, se respektive \navgång.Inställda avgångar kan förekomma."
+        # })
+        error, messages_json = api_train_message()
+        if error != None:
+            logger.error("Can't access API to get train messages.")
+            return
+        else:
+            for json in messages_json["RESPONSE"]["RESULT"][0]["TrainStationMessage"]:
+                if json["Status"] == "Lag":
+                    lag_message.append(json)
+                else:
+                    normal_message.append(json)
+        self.remove_children()
+        # TODO: The active days should be considered.
+        for msg in lag_message:
+            normalized_message = msg["FreeText"].replace("\n", "")
+            self.mount(MessageLabel(normalized_message, classes="lag"))
+            self.mount(Rule())
+        for msg in normal_message:
+            normalized_message = msg["FreeText"].replace("\n", "")
+            self.mount(MessageLabel(normalized_message, classes="normal"))
+            self.mount(Rule())
+        self.set_loading(False)
+
+    def get_visible_size(self):
+        return self.content_size
+    def on_mount(self):
+        self.border_title = "Train Notice"
+        self.set_loading(True)
+        self.load_message()
+        self.set_interval(config["message"]["updateIntervalMin"] * 60, self.load_message)
+    def refresh_message(self):
+        self.load_message()
+
+tz = pytz.UTC
+class TrainScheduleTable(DataTable):
+    schedule_cache = []
+    stations = []
+    def __init__(self, schedule, stations):
+        super().__init__()
+        self.schedule_cache = schedule
+        self.stations = stations
+
+    def refresh_data(self):
+        self.clear()
+        for row in self.schedule_cache:
+            time = datetime.fromisoformat(row["AdvertisedTimeAtLocation"])
+            now = datetime.now(tz=tz)
+            track = row["TrackAtLocation"]
+            from_station = "/".join([self.stations[loc] for loc in row["FromLocation"]])
+            if now > time:
+                # Train left
+                delta = (now - time).seconds
+                to_station = "/".join(self.stations[loc] for loc in row["ToLocation"])
+                line = "{f} :arrow_right: {t}".format(f=from_station, t=to_station)
+                # Make the left train gray
+                line = "[#808080]{line}[/#808080]".format(line=line)
+                track = "[#808080]{track}[/#808080]".format(track=track)
+                departure_time = "[#808080]{min}m ago[/#808080]".format(min=int(delta / 60))
+            else:
+                # Train arriving
+                delta = (time - now).seconds
+                to_station = "/".join(["[bold][#08852C]{loc}[/#08852C][/bold]".format(loc=self.stations[loc]) for loc in row["ToLocation"]])
+                line = "{f} :arrow_right: {t}".format(f=from_station, t=to_station)
+                departure_time = "{min} min".format(min=int(delta / 60))
+            self.add_row(line, track, departure_time)
+
+    def on_mount(self):
+        self.cursor_type = "none"
+        self.refresh_data()
+        self.set_interval(60, self.refresh_data)
+
+class TrainSchedule(Static):
+    stations = {}
+    last_refresh = datetime.min
+    def load_stations(self):
+        error, stations_json = api_train_stations()
+        if error != None:
+            logger.error("Can't access API to get train stations.")
+            return
+        else:
+            for json in stations_json["RESPONSE"]["RESULT"][0]["TrainStation"]:
+                self.stations[json["LocationSignature"]] = json["AdvertisedLocationName"]
+            logger.debug("Stations loaded: {stations}".format(stations=self.stations))
+    def load_schedule(self):
+        now = datetime.now()
+        hour = now.hour
+        cfg = [cfg for cfg in config["apiFreqControl"] if hour >= cfg["from"] and hour < cfg["to"]][0]
+        interval = cfg["intervalMin"]
+        delta = (now - self.last_refresh).seconds / 60
+        if delta < interval:
+            logger.info("Waiting for the next round to refresh.")
+            return
+        error, schedule_json = api_train_announcement()
+        if error != None:
+            logger.error("Can't access API to get train stations.")
+            return
+        else:
+            self.remove_children()
+            schedules = schedule_json["RESPONSE"]["RESULT"][0]["TrainAnnouncement"]
+            table = TrainScheduleTable(schedules, self.stations)
+            table.add_columns(*("Line", "Track", "Time"))
+            self.mount(table)
+            self.set_loading(False)
+        self.last_refresh = now
+    def on_mount(self):
+        self.border_title = "Train Schedule (Odenplan)"
+        self.set_loading(True)
+        self.load_stations()
+        self.load_schedule()
+        # Refresh stations on a daily basis.
+        self.set_interval(86400, self.load_stations)
+        self.set_interval(60, self.load_schedule)
+        # TODO: periodically load new schedule
+
+class TrainInfoApp(App):
+    BINDINGS = [
+        ("d", "toggle_dark_mode" ,"Toggle dark mode"),
+        ("q", "quit", "Quit"),
+        ("r", "refresh", "Refresh")
+    ]
+    CSS_PATH = "train_info.css"
+
+    def compose(self):
+        yield Header(show_clock=True)
+        with ScrollableContainer(id="layout"):
+            yield TrainSchedule(id="schedule")
+            yield TrainStationMessage(id="message")
+        yield Footer()
+
+    def action_toggle_dark_mode(self):
+        self.dark = not self.dark
+
+    def action_quit(self):
+        exit()
+    
+    def action_refresh(self):
+        self.query_one("TrainStationMessage").refresh_message()
+
+if __name__ == "__main__":
+    TrainInfoApp().run()
